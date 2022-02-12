fe41b8d4a2e442123fb64fc714d4b0b40879efe6
Aldo Larrain <aldo.larrain@hotmail.com>
2022-02-12 17:15:13 -0400

    Commit de prueba

---
+++ 02-.js
@@ -0,0 +1,36 @@
+//Variables let const (var)
+
+const PI = 3.1416
+
+ console.log(PI);
+
+ let nombre = "Vegeta";
+
+ console.log("Tu nombre es:", nombre);
+
+ nombre = "Goku"
+
+ console.log("Tu nombre es:", nombre);
+
+ //const 1hola = "Goku" no se hace
+ //const !var1 = "Goku" no se usan simbolos especiales
+ //const hola-var1
+
+ //_
+ const _user = "Trunks";
+
+ //$
+ const $sendEmail = true;
+
+ let _fruta="Manzana"
+ {
+  _fruta = "Kiwi"
+   console.log(_fruta)
+  }
+  
+  console.log(_fruta)
+
+  
+  const IS_ACTIVE = true,
+        IS_null = null;
+        
