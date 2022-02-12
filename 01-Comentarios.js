5123c148b563b59b18c1730534cea4a69fd9a1bd
Aldo Larrain <aldo.larrain@hotmail.com>
2022-02-12 17:15:41 -0400

    Commit prueba 2

---
+++ 01-Comentarios.js
@@ -0,0 +1,16 @@
+//Comentarios
+/* 
+  1-funcionalidad para enviar por email 
+  2.-Envio por sms
+*/
+
+/**
+ * 
+ * @param {string} email string email user 
+ * @returns string
+ */
+
+ function sendEmail(email) {
+    return "Email" + email;
+  }
+  
---
+++ 04-NOSE.js
@@ -0,0 +1,43 @@
+
+
+
+
+// // function verificaTipo(valor){
+// //   console.log(`${valor} es ${typeof valor}`);
+// // }
+
+
+// // verificaTipo("Cadena");
+// // verificaTipo(true);
+// // verificaTipo(false);
+// // verificaTipo(19.99);
+// // verificaTipo({});
+// // verificaTipo(1000n);
+
+// function typeValue(valor){
+//   return typeof valor
+// }
+
+// // const value = 10
+
+// // const isNumber = value === verificaTipo(value) === 'number'
+// // if(isNumber){
+// //   console.log("Si es un numero")
+// // }else{
+// //   console.log("No es un numero")
+// // }
+
+// function suma(a,b){
+//   if(typeValue(a)==="number" && typeValue(b)==="number"){
+//   return a + b
+//   }else if(typeValue(a)==="string" && typeValue(b)==="number"){
+//     return Number(a) + b;
+
+//   }
+  
+// }
+
+// const a =20;
+// const b=10;
+//  const res=suma(a,b);
+//  console.log(res)
